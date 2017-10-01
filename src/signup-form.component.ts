ec2883fe4e9e7282f4252f5f0603c6310bd793d5
Brain Tucker <brian.briantucker@gmail.com>
2017-10-01 10:24:07 -0500

    starting angular form validation, added error styling

--- src/signup-form.component.ts
+++ src/signup-form.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'signup-form',
   template: `
-    <form (submit)="onSubmit()">
+    <form (submit)="onSubmit()" novalidate>
       <div class="form-group">
         <label>Email</label>
         <input type="email" class="form-control"
@@ -11,7 +11,11 @@ import { Component } from '@angular/core';
       </div>
       <button type="submit" class="btn btn-primary">Sign Up</button>
     </form>
-  `
+  `,
+  styles: [`
+      input.ng-invalid {border: solid red 2px; }
+      input.ng-valid { border: solid green 2px; }
+  `]
 })
 export class SignupFormComponent {
 
